5ea224ff436a823809b3ed2face1e6dc9a5021a4
kamesh <kameshthehacker01@gmail.com>
2026-02-18 23:02:24 +0530

    Add files via upload

---
+++ internal/admin.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Internal Administration Panel</h2>";
+echo "<p>Access Level: ROOT</p>";
+echo "<hr>";
+echo "<h3>Security Token:</h3>";
+echo "<h1>nexus{hidden_admin_access}</h1>";
+?>
